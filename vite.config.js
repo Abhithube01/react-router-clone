563bdf6e0a077c6d44ba6c6bb3f8cbcd4a117f84
Abhinav Anil Thube <abhinavthube@gmail.com>
2024-04-14 19:14:24 +0530

    save

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://Abhithube01.github.io/react-router-clone',
   plugins: [react()],
 })
